ce446b9ed792617f8f783e8c1430f41dc692bdeb
Samantha Paez <sampaez@udel.edu>
2026-02-28 13:59:08 -0500

    form changes depending on type of user

--- login/makeAccount.js
+++ login/makeAccount.js
@@ -3,32 +3,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("makeAccountForm");
     const errorMessage = document.getElementById("errorMessage");
   
+    const userType = document.getElementById("userType");
+    const govFields = document.getElementById("governmentFields");
+    const citizenFields = document.getElementById("citizenFields");
+    const citySelect = document.getElementById("citySelect");
+  
+    // Show / hide form fields depending on type of user
+    userType.addEventListener("change", async function () {
+  
+      if (this.value === "government") {
+        govFields.style.display = "block";
+        citizenFields.style.display = "none";
+      }
+  
+      else if (this.value === "citizen") {
+        govFields.style.display = "none";
+        citizenFields.style.display = "block";
+      }
+    });
+  
+    // Submit form
     form.addEventListener("submit", async function (event) {
       event.preventDefault();
   
       const email = document.getElementById("email").value;
-      const city = document.getElementById("city").value;
       const password = document.getElementById("password").value;
   
-      try {
-        const response = await fetch("http://localhost:5000/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ email, city, password })
-        });
+      let city;
   
-        if (!response.ok) {
-          throw new Error("Failed to create account");
-        }
-  
-        alert("Account created successfully!");
-        window.location.href = "login.html";
-  
-      } catch (error) {
-        errorMessage.textContent = error.message;
+      if (userType.value === "government") {
+        city = document.getElementById("cityInput").value;
+      } else {
+        city = document.getElementById("citySelect").value;
       }
+  
+
+      let state;
+  
+      if (userType.value === "government") {
+        city = document.getElementById("stateInput").value;
+      } else {
+        city = document.getElementById("stateSelect").value;
+      }
+      alert("Account created successfully!");
+
+      window.location.href = "loginPage.html";
     });
   
   });
