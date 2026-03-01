09608de772fbc1ccf71e4bd61c1030288aac86b2
nolanKeefe <npkeefe@udel.edu>
2026-03-01 01:40:23 -0500

    lasso button yay

---
+++ Settings/settings.js
@@ -0,0 +1,4 @@
+function toggleImage() {
+    let image = document.getElementById("settings-gif"); // Get the image element by its ID
+    image.classList.toggle("hidden"); // Toggle the "hidden" class
+}
